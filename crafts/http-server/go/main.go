18c7b320f4c50bd5ecc5faadab7504dc4f821280
tomo-local <tomohiro.togashi@gmail.com>
2026-05-18 00:55:23 +0900

    接続元アドレスの取得を追加

--- crafts/http-server/go/main.go
+++ crafts/http-server/go/main.go
@@ -7,21 +7,24 @@ import (
 )
 
 func main() {
+	// サーバーの立ち上げ
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Println("Listening on :8080")
 	defer ln.Close()
 
-	fmt.Println("Listening on :8080")
-
 	for {
+		// 接続確率
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		fmt.Printf("conn: %v\n", conn)
+
+		add := conn.RemoteAddr()
+		fmt.Printf("add: %v\n", add)
 	}
 }
