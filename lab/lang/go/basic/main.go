caf8d96978cf7063238b6eaad0095e2f72f082ee
tomo-local <tomohiro.togashi@gmail.com>
2026-07-21 23:09:25 +0900

    study:typeとstructについての学習

--- lab/lang/go/basic/main.go
+++ lab/lang/go/basic/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	john := Human{
+		name: "John Done",
+		age:  30,
+		city: "Tokyo",
+	}
+
+	john.greet()
+}
+
+type Human struct {
+	name string
+	age  uint8
+	city string
+}
+
+func (h *Human) greet() {
+	fmt.Printf("I am %s, %d, and I am from %s\n\r", h.name, h.age, h.city)
+}
